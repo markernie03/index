1cc8bef70dca30b6d94cb280dfe4b4cd9dbeb563
Mark Ernie <143605896+markernie03@users.noreply.github.com>
2023-08-30 21:55:51 +0800

    image

--- app/page.js
+++ app/page.js
@@ -78,7 +78,7 @@ export default function Home() {
     return (
       <div className="flex flex-col md:flex-row items-center justify-center md:gap-4 mt-10 md:mt-40 pb-10 ">
         <img
-          src="https://scontent.fcrk3-2.fna.fbcdn.net/v/t39.30808-6/359746489_6714841381899280_5397832173784774151_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeH9_SPRQEbty48gFK3kXZTsV31ojF4rvrpXfWiMXiu-ulZQuCdK6YBYr2T9g-hg1WLOOvJZDyQ6hMOAyzbgfxiM&_nc_ohc=01d9ENiqRFIAX-abias&_nc_zt=23&_nc_ht=scontent.fcrk3-2.fna&oh=00_AfAjhEsDVT9eAdn5TVSP76OFlnjhK528qxh1LZeMeWn3jA&oe=64F45E9D"
+          src="https://scontent.xx.fbcdn.net/v/t39.30808-6/369189664_6857873450929405_8203080556271658833_n.jpg?stp=dst-jpg_s960x960&_nc_cat=103&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeFm520b58QYLJvADahyN5opG3ljuXbE5CUbeWO5dsTkJZIeUI76ENYAlN99UzCGrOyYbhSQOeXLl4S1LLhRJFnC&_nc_ohc=tSyb_ijjCXUAX-Vx6aa&_nc_ht=scontent.xx&oh=00_AfCtAZE7fd4QvCH7S_BpKUvOyWuG4fw5BAQ0vWTDs9WB_w&oe=64F410E2"
           alt="Developer profile image"
           style={{
             height: "500px",
