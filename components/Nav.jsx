a6f92cd9423a9ada98ecc104770247063e632003
Mark Ernie <143605896+markernie03@users.noreply.github.com>
2023-08-31 12:43:53 +0800

    commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
     <nav className="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 md:p-8">
-        <Link href="" className="flex items-center no-underline text-black">
+        <Link href="./" className="flex items-center no-underline text-black">
           <span className="self-center text-2xl font-semibold whitespace-nowrap">
             ErnieWebDev
           </span>
