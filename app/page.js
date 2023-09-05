4bf097b968631cef07a27c4ecfa7593de86d5ddd
Mark Ernie <143605896+markernie03@users.noreply.github.com>
2023-09-05 11:38:33 +0800

    changes

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
-import ProjectCard from "@/components/ProjectCard";
-import Image from "next/image";
+import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
+import About from "@/components/About";
 
 
 export default function Home() {
